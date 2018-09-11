d73418f8a23fa49efddd16182dbe003b981d3034
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-11 04:41:19 -0700

    Byte-to-string decoding

--- puppet.py
+++ puppet.py
@@ -6,7 +6,7 @@ def clean_puppet_cert(vm):
     container = client.containers.get('puppet')
     output = container.exec_run(f'puppet cert clean {vm}.aperture.com')
 
-    if 'Revoked certificate' in output.output:
+    if 'Revoked certificate' in output.output.decode("utf-8"):
         return { 'status': 'success' }
 
     return { 'status': 'fail' }
