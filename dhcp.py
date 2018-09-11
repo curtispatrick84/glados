b4b58fd7e2e50af63518595dca96616f06f53e03
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-11 14:58:13 -0700

    Debugging DHCP reservation file creation

--- dhcp.py
+++ dhcp.py
@@ -11,9 +11,7 @@ def update_reservation(vm, ip, mac):
 
     newfile = '\n'.join(file)
 
-    print(newfile)
-
-    container.exec_run(f'echo -e {newfile} > /etc/dhcp/dhcpd.conf')
+    container.exec_run('sh', '-c', f'echo -e {newfile} > /etc/dhcp/dhcpd.conf')
     container.restart()
 
     return { 'success': True }
