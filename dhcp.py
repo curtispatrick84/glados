c3eb5b97551ca92b782a3e8a00de26e4f9ff7fdb
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-11 14:49:58 -0700

    Debugging DHCP reservation file creation

--- dhcp.py
+++ dhcp.py
@@ -4,12 +4,14 @@ client = docker.from_env()
 
 def update_reservation(vm, ip, mac):
     container = client.containers.get('dhcp')
-    output = container.exec_run(f'cat /etc/dhcp/dhcpd.conf')
+    output = container.exec_run('cat /etc/dhcp/dhcpd.conf')
 
     file = output.output.decode('utf-8').splitlines()
     file.insert(14, f'    host {vm} {{ hardware ethernet {mac}; fixed-address {ip}; }}')
 
     newfile = '\n'.join(file)
+
+    print(newfile)
 
     container.exec_run(f'echo -e {newfile} > /etc/dhcp/dhcpd.conf')
     container.restart()
