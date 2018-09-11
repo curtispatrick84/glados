6b13d7b0d54f1ddb71d2a4525167772449a52da6
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-11 06:10:31 -0700

    mq.py tweaks

--- mq.py
+++ mq.py
@@ -3,12 +3,10 @@ import json
 
 from config import Config
 
-parameters = pika.ConnectionParameters(Config.RABBIT_MQ_SERVER)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue='provisioning')
-
-channel.basic_publish(exchange='', routing_key='provisioning', body="starting up...")
-
 def publish(msg):
+    parameters = pika.ConnectionParameters(Config.RABBIT_MQ_SERVER)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='provisioning')
     channel.basic_publish(exchange='', routing_key='provisioning', body=json.dumps(msg))
+    connection.close()
