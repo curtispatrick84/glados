21e3476851ea286b1ef84197a8457d0539f204e1
Curtis Patrick <curtis.patrick84@gmail.com>
2018-09-11 14:43:22 -0700

    Adding DHCP reservation update

---
+++ dhcp.py
@@ -0,0 +1,17 @@
+import docker
+
+client = docker.from_env()
+
+def update_reservation(vm, ip, mac):
+    container = client.containers.get('dhcp')
+    output = container.exec_run(f'cat /etc/dhcp/dhcpd.conf')
+
+    file = output.output.decode('utf-8').splitlines()
+    file.insert(14, f'    host {vm} { hardware ethernet {mac}; fixed-address {ip}; }')
+
+    newfile = '\n'.join(file)
+
+    container.exec_run(f'echo -e {newfile} > /etc/dhcp/dhcpd.conf')
+    container.restart()
+
+    return { 'success': True }
